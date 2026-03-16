5a64ae26baa2723e5435ed29a372682b292b0dbc
Arya Baranwal <baranwalarya414@gmail.com>
2026-03-16 23:15:51 +0530

    update code

--- frontend/src/hooks/useGetLocation.jsx
+++ frontend/src/hooks/useGetLocation.jsx
@@ -1,26 +1,58 @@
 import axios from "axios";
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { setCurrentAddress, setCurrentCity, setCurrentState, setUserData } from "../redux/userSlice";
+import { setCurrentAddress, setCurrentCity, setCurrentState } from "../redux/userSlice";
 import { setAddress, setLocation } from "../redux/mapSlice";
 
 function useGetLocation() {
-    const dispatch=useDispatch()
-    const {userData} = useSelector(state=>state.user)
-    const apiKey=import.meta.env.VITE_GEOAPIKEY
-  useEffect(()=>{
-    navigator.geolocation.getCurrentPosition(async (position)=>{
-        const lat=position.coords.latitude
-        const lon=position.coords.longitude
-        dispatch(setLocation({lat:lat,lon:lon}))
-        const result=await axios.get(`https://api.geoapify.com/v1/geocode/reverse?lat=${lat}&lon=${lon}&format=json&apiKey=${apiKey}`)
-        dispatch(setCurrentCity(result?.data?.results[0].city))
-        dispatch(setCurrentState(result?.data?.results[0].state))
-        dispatch(setCurrentAddress(result?.data?.results[0].address_line1 + ","
-        + result?.data?.results[0].address_line2))
-        dispatch(setAddress(result?.data?.results[0].formatted))
-    })
-  },[userData])
+  const dispatch = useDispatch();
+  const { userData } = useSelector((state) => state.user);
+  const apiKey = import.meta.env.VITE_GEOAPIKEY;
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(async (position) => {
+      const lat = position.coords.latitude;
+      const lon = position.coords.longitude;
+
+      dispatch(setLocation({ lat, lon }));
+
+      try {
+        const result = await axios.get(
+          "https://api.geoapify.com/v1/geocode/reverse",
+          {
+            params: {
+              lat: lat,
+              lon: lon,
+              format: "json",
+              apiKey: apiKey
+            },
+            withCredentials: false   // ⭐ IMPORTANT FIX
+          }
+        );
+
+        const location = result?.data?.results?.[0];
+
+        if (!location) return;   // ⭐ crash prevent
+
+        dispatch(setCurrentCity(location.city));
+        dispatch(setCurrentState(location.state));
+
+        dispatch(
+          setCurrentAddress(
+            (location.address_line1 || "") +
+            "," +
+            (location.address_line2 || "")
+          )
+        );
+
+        dispatch(setAddress(location.formatted));
+
+      } catch (error) {
+        console.log("Geoapify error:", error);
+      }
+
+    });
+  }, [userData]);
 }
 
-export default useGetLocation
+export default useGetLocation;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,14 @@
 // import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom"
-import axios from "axios"
+
 import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import { store } from './redux/store.js'
 
 
-axios.defaults.withCredentials = true
+
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <Provider store={store}>
